3b71355e9df1de0e51a55c6c6e81f1b69cc349c8
Gabriel Crépeault-Cauchon <gabrielcrepeault@gmail.com>
2018-04-06 22:56:46 -0400

    Debug package

--- R/TVaR_beta.R
+++ R/TVaR_beta.R
@@ -1,13 +1,13 @@
 #' Tail Value-at-Risk  d'une loi Beta
 #' @param a alpha
 #' @param b beta
-#' @param k kappa (doit être entre 0 et 1)
+#' @param k kappa doit être entre 0 et 1
 #' @export
 TVaR_beta <- function(k, a, b)
 {
     fbeta <- a / (a+b)
     ftvar <- 1 / (1-k)
-    fbeta * ftvar * (1 - pbeta(VaR_beta(k, a, b), a+1, b))
+    fbeta * ftvar * (1 - pbeta(qbeta(k, a, b), a+1, b))
 }
 
 
