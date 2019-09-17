90b95dd18f22062450d4d7be5056cf24d4627f18
Alec James van Rassel <alec.van-rassel.1@ulaval.ca>
2019-09-17 13:15:59 -0400

    Ajout binneg
    Ajout de la fonction de survie à la fonction de répartition de la binomiale négative.

--- R/p_negbinom.R
+++ R/p_negbinom.R
@@ -4,8 +4,16 @@
 #' @param p Probabilité de succès.
 #' @param beta Deuxième paramétrisation de la loi binomiale négative tel que beta = (1 - p) / p.
 #' @param nb_tries Stipule si la binomiale est une du nombre d'essais pour le r-ème succès, ou du nombre d'échecs pour le r-ème succès (par défaut).
+#' @param lower.tail Si vrai (défaut) alors fonction de répartition. Si faux, de survie.
 #' @export
-p_negbinom <- function(k, r, p = (1 / (1 + beta)), beta = ((1 - p) / p), nb_tries = F)
+p_negbinom <- function(k, r, p = (1 / (1 + beta)), beta = ((1 - p) / p), nb_tries = F, lower.tail = F)
 {
-    sum(sapply(0:k, function(i) d_nbinom(k = i, r, p, nb_tries)))
+    if(lower.tail)
+    {
+        sum(sapply(0:k, function(i) d_nbinom(k = i, r, p, nb_tries)))
+    }
+    else
+    {
+        1 - sum(sapply(0:k, function(i) d_nbinom(k = i, r, p, nb_tries)))
+    }
 }
