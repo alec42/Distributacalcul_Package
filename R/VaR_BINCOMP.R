bcd990c6a00cf3ed6164211e979e45cfc2539f49
Alec James van Rassel <alec.van-rassel.1@ulaval.ca>
2019-09-19 12:44:50 -0400

    Correction lois composées

--- R/VaR_BINCOMP.R
+++ R/VaR_BINCOMP.R
@@ -2,11 +2,13 @@
 #' @param kappa Niveau de confiance désiré
 #' @param n n pour la binomiale
 #' @param q probabilité q pour la binomiale
-#' @param shape alpha pour la Gamma et mu pour la lognormale
-#' @param rate beta pour la Gamma et sigma^2 pour la lognormale
+#' @param shape alpha pour la Gamma
+# et mu pour la lognormale
+#' @param rate beta pour la Gamma
+# et sigma^2 pour la lognormale
 #' @param ko borne pour l'optimisation numérique de la VaR
-#' @param distr_severity choix de distribution de sévérité. Gamma ou Lognormale
-#' @details Cette formule utilise aussi un choix de 2 distributions pour la sévérité; soit la Gamma (par défaut) ou la Lognormale.
+#' @param distr_severity choix de distribution de sévérité. Gamma uniquement pour l'instant.
+# @details Cette formule utilise aussi un choix de 2 distributions pour la sévérité; soit la Gamma (par défaut) ou la Lognormale.
 #' @export
 VaR_BINCOMP <- function(kappa, n, q, shape, rate, ko = 300, distr_severity = "Gamma")
 {
--- R/p_PCOMP.R
+++ R/p_PCOMP.R
@@ -1,11 +1,14 @@
 #' Fonction de répartition d'une loi poisson composée
 #' @param x x
 #' @param lambda paramètre lambda pour la poisson
-#' @param shape alpha pour la Gamma et mu pour la lognormale
-#' @param rate beta pour la Gamma et sigma^2 pour la lognormale
+#' @param shape alpha pour la Gamma
+#  et mu pour la lognormale
+#' @param rate beta pour la Gamma
+#  et sigma^2 pour la lognormale
 #' @param ko borne pour la sommation de la fonction de répartition
-#' @param distr_severity choix de distribution de sévérité. Gamma (par défaut) ou Lognormale
-#' @details Cette formule utilise aussi un choix de 2 distributions pour la sévérité; soit la Gamma ou la Lognormale.
+#' @param distr_severity choix de distribution de sévérité.
+# Gamma (par défaut) ou Lognormale
+# @details Cette formule utilise aussi un choix de 2 distributions pour la sévérité; soit la Gamma ou la Lognormale.
 #' @export
 p_PCOMP <- function(x, lambda, shape, rate, ko = 300, distr_severity = "Gamma"){
 
@@ -14,8 +17,9 @@ p_PCOMP <- function(x, lambda, shape, rate, ko = 300, distr_severity = "Gamma"){
     {
         (dpois(x = 0, lambda) + sum(sapply(1:ko, function(k) dpois(x = k, lambda) * pgamma(q = x, shape * k, rate))))
     }
-    else if (distr_severity == "Lognormale")
-    {
-        dpois(x = 0, lambda) + sum(sapply(1:ko, function(k) dpois(x = k, lambda) * plnorm(q = x, meanlog = shape * k, sdlog = sqrt(rate))))
-    }
+    # pas bon
+    # else if (distr_severity == "Lognormale")
+    # {
+    #     dpois(x = 0, lambda) + sum(sapply(1:ko, function(k) dpois(x = k, lambda) * plnorm(q = x, meanlog = shape * k, sdlog = sqrt(rate))))
+    # }
 }
