9a51c7371831d2b78c4d3be419cfa797d047443a
Alec James van Rassel <alec.van-rassel.1@ulaval.ca>
2019-10-09 14:36:08 -0400

    amélioration de la binneg

--- R/p_negbinom.R
+++ R/p_negbinom.R
@@ -6,23 +6,17 @@
 #' @param nb_tries Stipule si la binomiale est une du nombre d'essais pour le r-ème succès, ou du nombre d'échecs pour le r-ème succès (par défaut).
 #' @param lower.tail Si vrai (défaut) alors fonction de répartition. Si faux, de survie.
 #' @export
-p_negbinom <- function(k, r, p = (1 / (1 + beta)), beta = ((1 - p) / p), nb_tries = F, lower.tail = T)
-{
-    if(nb_tries)
-        start_FX <- r
-    else
-        start_FX <- 0
-
-    if(nb_tries & k < r)
-        return(NULL)
-    else
+p_negbinom <- function(k, r, p = (1 / (1 + beta)), beta = ((1 - p) / p), nb_tries = FALSE, lower.tail = T)
     {
-        if(lower.tail)
-        {
+    start_FX <- ifelse(nb_tries, r, 0)
+
+    if((nb_tries = T) & (k < r)){
+        return(NULL)}
+    else{
+        if(lower.tail){
             sum(sapply(start_FX:k, function(i) d_negbinom(k = i, r = r, p = p, nb_tries = nb_tries)))
         }
-        else
-        {
+        else{
             1 - sum(sapply(start_FX:k, function(i) d_negbinom(k = i, r = r, p = p, nb_tries = nb_tries)))
         }
     }
