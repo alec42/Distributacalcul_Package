6bd22e2aa1ad1e9ac5d0dc981a708f59cc860e8c
alec42 <alecvanrassel@icloud.com>
2020-05-20 13:21:08 -0400

    Update Etronq_IG.R
    Added truncation for IG distribution

--- R/Etronq_IG.R
+++ R/Etronq_IG.R
@@ -4,6 +4,7 @@
 #'  mean \eqn{\mu}{mu} and shape parameter \eqn{\beta}{beta}.
 #'
 #' @templateVar d TRUE
+#' @templateVar less.than.d TRUE
 #' @template IG-template
 #'
 #' @export
@@ -13,10 +14,10 @@
 #'
 #' Etronq_IG(d = 2, mean = 2, shape = 5)
 #'
-Etronq_IG <- function(d, mean, shape = dispersion * mean^2, dispersion = shape / mean^2) {
+Etronq_IG <- function(d, mean, shape = dispersion * mean^2, dispersion = shape / mean^2, less.than.d = TRUE) {
     stopifnot(d > 0, mean >= 0, shape > 0)
 
-    d -
+    Etronq.IG.less.than.d <- d -
         (2 * d - mean) *
         stats::pnorm(q = (d - mean) *
                   sqrt(1 / (shape * d))) -
@@ -24,4 +25,10 @@ Etronq_IG <- function(d, mean, shape = dispersion * mean^2, dispersion = shape /
         exp(2 * mean / shape) *
         stats::pnorm(q = - (d + mean) *
                   sqrt(1 / (shape * d)))
+
+    if (less.than.d) {
+        return(Etronq.IG.less.than.d)
+    } else {
+        return(mean - Etronq.IG.less.than.d)
+    }
 }
